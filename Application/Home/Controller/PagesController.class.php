86d03e19ed512c1bb0184342a06118c00718dc75
wanglu <wanglu@happyjuzi.com>
2016-04-23 16:37:17 +0800

    模版修改

---
+++ Application/Home/Controller/PagesController.class.php
@@ -0,0 +1,24 @@
+<?php
+namespace Home\Controller;
+use Home\Controller\CommonController;
+use Think\Controller;
+
+class PagesController extends CommonController {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	//显示管理员列表
+	public function index() {
+
+		//dump($_SESSION['node']);
+		$this->pagesList();
+	}
+
+	//显示所有管理员
+	public function pagesList() {
+        $this->display('pagesList');
+	}
+
+}
